6c88e62b0b6ee47c9e2b83f1345c1f578fdaeab1
KevGalindo <163373098+KevGalindo@users.noreply.github.com>
2024-04-24 21:03:13 -0500

    Update dashboard_conductor.js

--- js/dashboard_conductor.js
+++ js/dashboard_conductor.js
@@ -1,8 +1,5 @@
-// dashboard.js
-// dashboard_conductor.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Simular datos de usuarios y montos de viajes (reemplaza esto con tu propia lógica de obtención de datos)
     const usuarios = [
         { id: 1, nombre: 'Usuario 1', viajes: 10 },
         { id: 2, nombre: 'Usuario 2', viajes: 8 },
@@ -11,14 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: 5, nombre: 'Usuario 5', viajes: 2 }
     ];
 
-    const montoTotal = 500; // Simular monto total de viajes
+    const montoTotal = 500; 
 
-    // Mostrar la lista de usuarios que más abordaron el vehículo
+
     const usuariosAbordaronContainer = document.getElementById('usuarios-abordaron');
-    usuariosAbordaronContainer.innerHTML = ''; // Limpiar contenido actual
+    usuariosAbordaronContainer.innerHTML = ''; 
 
     usuarios.forEach(usuario => {
-        // Crear elementos HTML para mostrar la información del usuario
+       
         const usuarioElement = document.createElement('div');
         usuarioElement.innerHTML = `
             <p><strong>Nombre:</strong> ${usuario.nombre}</p>
@@ -27,19 +24,18 @@ document.addEventListener('DOMContentLoaded', function() {
         usuariosAbordaronContainer.appendChild(usuarioElement);
     });
 
-    // Mostrar el monto total de los viajes realizados
+    
     const montoTotalContainer = document.getElementById('monto-total');
     montoTotalContainer.innerHTML = `<p><strong>Monto Total:</strong> ${montoTotal}</p>`;
     
-    // Manejar evento de clic en el botón "Salir"
+   
     document.getElementById('salir').addEventListener('click', function() {
-        // Limpiar el localStorage y redirigir al usuario a la página de inicio de sesión
+   
         localStorage.removeItem('userId');
         window.location.href = 'index.html';
     });
 });
 
-// Función para cerrar sesión
 function logout() {
     // Eliminar el userType y userId del localStorage
     localStorage.removeItem('userType');
@@ -48,18 +44,18 @@ function logout() {
     window.location.href = 'index.html';
 }
 
-// Obtener referencias a los elementos del DOM
+
 const logoutButtonUser = document.getElementById('logout-user');
 const logoutButtonDriver = document.getElementById('logout-driver');
 
-// Manejar evento de clic en el botón de salir para usuarios
+
 if (logoutButtonUser) {
     logoutButtonUser.addEventListener('click', () => {
         logout();
     });
 }
 
-// Manejar evento de clic en el botón de salir para conductores
+
 if (logoutButtonDriver) {
     logoutButtonDriver.addEventListener('click', () => {
         logout();
